6727770466d3816fa15f830d033ed5c57468b9bc
Spencer Kenison <158218891+stkenison@users.noreply.github.com>
2024-11-25 13:18:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,27 +15,23 @@ theta = np.linspace(0,np.pi,N) #calculate theta values based off number of nodes
 z_b = -0.5*np.cos(theta)
 c_b = 2*(1-(1-R_T)*np.abs(np.cos(theta)))/(R_A*(1+R_T)) #chord length normalized by span
 
-#plot airfoil for verification
-plt.figure('Planform Plot'); plt.plot(z_b,c_b/4, color = 'blue')
-plt.plot(z_b,-3*c_b/4, color = 'blue')
-plt.xlabel("Spanwise Position (z/b)")
-plt.ylabel("Chord Position (c/b)")
-plt.plot([z_b[0],z_b[0]],[-3*c_b[0]/4,c_b[0]/4],color = 'blue'); plt.plot([z_b[-1],z_b[-1]],[-3*c_b[-1]/4,c_b[-1]/4],color = 'blue')
-for i in range(N):
-    plt.plot([z_b[i],z_b[i]],[-3*c_b[i]/4,c_b[i]/4],color = 'red', linestyle = '--')
-plt.axis("equal")
-plt.grid(True)
-plt.show()
-
+#define washout distribution
 washout_distribution_type = data['wing']['washout']['distribution'] #get washout distribution type
 if washout_distribution_type == 'optimum':
     omega = 1-np.sin(theta)/(c_b/c_b[N//2]) #define optimum washout distribution
 elif washout_distribution_type == 'linear':
-    omega = 1-np.sin(theta)
+    omega = np.abs(np.cos(theta)) #define linear washout distribution, eqn 6.30
 else: 
     omega = np.zeros(N)
-washout = np.radians(data['wing']['washout']['amount[deg]']) #Load washout amount from input file
 
+#define washout based off of inputs
+if type(data['wing']['washout']['amount[deg]']) == int or type(data['wing']['washout']['amount[deg]']) == float:
+    washout_amount = np.radians(data['wing']['washout']['amount[deg]']) #Load washout amount from input file
+else:
+    washout_amount = 2*(R_T+1)*data['wing']['washout']['CL_design']/(np.pi*C_La_2D) #define washout based on optimum washout distribution for wings with linear taper
+print('washout = ', washout_amount)
+
+#create C and C inverse arrays
 C = np.zeros((N,N)) #initialize C array
 for i in range(N):
     for j in range(N):
@@ -46,9 +42,39 @@ for i in range(N):
             C[i,j] = ((-1)**(n+1))*(n**2)
         else:
             C[i,j] = (4/C_La_2D/c_b[i]+n/np.sin(theta[i]))*np.sin(n*theta[i])
-C_inv = np.linalg.inv(C) #POSSIBLE ISSUES
+C_inv = np.linalg.inv(C)
+
+#solve for a and b matrixes used for coefficient solutions
 a = np.matmul(C_inv,np.ones(N))
 b = np.matmul(C_inv,omega)
+
+#define theta_f for aileron distribution
+theta_f = np.zeros(N)
+begin_z_b = data['wing']['aileron']['begin[z/b]']
+end_z_b = data['wing']['aileron']['end[z/b]']
+begin_cf_c = data['wing']['aileron']['begin[cf/c]']
+end_cf_c = data['wing']['aileron']['end[cf/c]']
+for i in range(N):
+    if (z_b[i] > begin_z_b and z_b[i] < end_z_b):
+        cf_c = begin_cf_c+(abs(z_b[i])-begin_z_b)*(end_cf_c-begin_cf_c)/(end_z_b-begin_z_b)
+        theta_f[i] = np.arccos(2*cf_c-1)
+    elif (-z_b[i] > begin_z_b and -z_b[i] < end_z_b):
+        cf_c = begin_cf_c+(abs(z_b[i])-begin_z_b)*(end_cf_c-begin_cf_c)/(end_z_b-begin_z_b)
+        theta_f[i] = np.arccos(2*cf_c-1)
+
+#define aileron distribution
+chi = np.zeros(N) #initialize aileron distribution
+for i in range(N):
+    if (z_b[i] > begin_z_b and z_b[i] < end_z_b):
+        chi[i] = -data['wing']['aileron']['hinge_efficiency']*(1-(theta_f[i]-np.sin(theta_f[i]))/(np.pi))
+    elif (-z_b[i] > begin_z_b and -z_b[i] < end_z_b):
+        chi[i] = data['wing']['aileron']['hinge_efficiency']*(1-(theta_f[i]-np.sin(theta_f[i]))/(np.pi)) 
+
+#calculate c_n matrix
+c = np.matmul(C_inv,chi)
+
+#calculate d_n matrix
+d = np.matmul(C_inv,np.cos(theta))
 
 #calculate kappa_L value
 kappa_L = (1-(1+R_A*np.pi/C_La_2D)*a[0])/((1+R_A*np.pi/C_La_2D)*a[0])
@@ -57,8 +83,7 @@ kappa_L = (1-(1+R_A*np.pi/C_La_2D)*a[0])/((1+R_A*np.pi/C_La_2D)*a[0])
 C_La_3D = np.pi*R_A*a[0]
 
 #calculate epsilon_omega value
-epsilon_omega = b[0]/a[0]
-print(epsilon_omega) ##COULD HAVE ISSUES
+epsilon_omega = b[0]/a[0] ##COULD HAVE ISSUES
 
 #calculate kappa_D value
 kappa_D = 0 #initialize kappa_D
@@ -78,111 +103,93 @@ for i in range(1,N): #sum kappa_Domega values
 #calculate kappa_D0 value
 kappa_D0 = kappa_D-(kappa_DL**2/(4*kappa_Domega))
 
+#calculate C_lp value
+C_lda = -np.pi*R_A*c[1]/4
+
+#calculate C_lp value 
+C_lp = -np.pi*R_A*d[1]/4
+
+#calculate coefficient of lift
+if type(data['condition']['alpha_root[deg]']) == int or type(data['condition']['alpha_root[deg]']) == float:
+    alpha_root = np.radians(data['condition']['alpha_root[deg]'])
+    C_L = C_La_3D*(alpha_root-epsilon_omega*washout_amount)
+else:
+    C_L = data['wing']['condition']['CL']
+    alpha_root = np.radians(1) #calculate root angle of attack
+
+#calculate induced drag coefficient
+C_Di = 1
+
+#calculate rolling moment coefficient
+C_l = 1
+
+#calculate yawing moment coefficient
+C_n = 1
+
+#calculate steady dimenionless roll rate
+p_steady = 1
+
+
+
+#write output matrixes to external file
 with open('output.txt', 'w') as f:
     f.write('C = \n\n'); np.savetxt(f, C, delimiter=', ', fmt='%.5f')
     f.write('\n\n\nC_inv = \n\n'); np.savetxt(f, C_inv, delimiter=', ', fmt='%.5f')
-    f.write('\n\n\na_n = \n\n'); np.savetxt(f, a, delimiter=', ', fmt='%.5f')
-    f.write('\n\n\nb_n = \n\n'); np.savetxt(f, b, delimiter=', ', fmt='%.5f')
+    f.write('\n\n\na_n = \n\n'); np.savetxt(f, a, delimiter=', ', fmt='%.7f')
+    f.write('\n\n\nb_n = \n\n'); np.savetxt(f, b, delimiter=', ', fmt='%.7f')
+    f.write('\n\n\nc_n = \n\n'); np.savetxt(f, c, delimiter=', ', fmt='%.7f')
+    f.write('\n\n\nd_n = \n\n'); np.savetxt(f, d, delimiter=', ', fmt='%.7f')
 
+#print calculated constant values to terminal
+print("K_L =",kappa_L)
+print("C_L,\u03B1 =",C_La_3D)
+print("\u03B5_\u03A9 =", epsilon_omega,"COULD HAVE ISSUES") 
+print("K_D =",kappa_D)
+print("K_DL =",kappa_DL)
+print("K_D\u03A9 = =",kappa_Domega)
+print("K_Do =",kappa_D0)
+print("C_l,\u03B4\u03B1 =",C_lda)
+print("C_l,\u0070\u0304 =",C_lp,"\n")
 
+#print calculated perfomance values to terminal
+print("C_L =",C_L)
+print("C_Di",C_Di)
+print("C_l =",C_l)
+print("C_n =",C_n)
+print("\u0070\u0304 =",p_steady,"\n")
 
+if data['view']['planform']: #plot planform if desired by user
+    plt.figure('Planform Plot'); plt.plot(z_b,c_b/4, color = 'blue')
+    plt.plot(z_b,-3*c_b/4, color = 'blue')
+    plt.xlabel("Spanwise Position (z/b)")
+    plt.ylabel("Chord Position (c/b)")
+    plt.plot([z_b[0],z_b[0]],[-3*c_b[0]/4,c_b[0]/4],color = 'blue'); plt.plot([z_b[-1],z_b[-1]],[-3*c_b[-1]/4,c_b[-1]/4],color = 'blue')
+    for i in range(N):
+        plt.plot([z_b[i],z_b[i]],[-3*c_b[i]/4,c_b[i]/4],color = 'red', linestyle = '--')
+    plt.axis("equal")
+    plt.grid(True)
+    right_aileron = np.array([[begin_z_b,end_z_b,end_z_b,begin_z_b,begin_z_b],[-1/R_A*3/4,-1/R_A*3/4,-1/R_A*3/4*(1-end_cf_c),-1/R_A*3/4*(1-begin_cf_c),-1/R_A*3/4]])
+    plt.plot(right_aileron[0,:],right_aileron[1,:],color = 'green')
+    plt.plot(-right_aileron[0,:],right_aileron[1,:],color = 'green') #flip aileron coordinate array for left side
 
-'''
-#initialize variables
-geometry = np.loadtxt(data['geometry'],dtype = float) #Load airfoil geometry from file
-alpha = np.array(data['alpha[deg]'])/180*np.pi #define desired angles of attack
-if np.isscalar(alpha): alpha = np.array([alpha])  # Convert scalar to an array if singular value
-v_inf = data['freestream_velocity'] #define freestream velocity
-n = geometry.shape[0] #count number of points in airfoil geometry file
-n_alpha = len(alpha) #count number of unique values in angle of attack list
+if data['view']['washout_distribution']: #plot washout distribution if desired by user
+    plt.figure('Washout Distribution')
+    plt.plot(z_b,omega, color = 'blue')
+    plt.grid(True)
+    plt.ylabel("Washout Distribution (\u03C9(z))")
+    plt.xlabel("Spanwise Position (z/b)")
 
-#initialize variables to describe geometry
-CP = 0.5*(geometry[1:]+geometry[:-1])#define control points
-dx = geometry[1:,0]-geometry[:-1,0] #define dx values
-dy = geometry[1:,1]-geometry[:-1,1] #define dy values
-l = np.sqrt(dx**2+dy**2) #define lengths of panels
+if data['view']['aileron_distribution']: #plot aileron distribution if desired by user
+    plt.figure('Aileron Distribution')
+    plt.plot(z_b,chi, color = 'blue')
+    plt.grid(True)
+    plt.ylabel("Aileron Position (\u03A7(z))")
+    plt.xlabel("Spanwise Position (z/b)")
 
-#solve for A array
-A = np.zeros((n,n)) #initialize A array
-for i in range(n-1):
-    for j in range(n-1):
-        xi = (dx[j]*(CP[i,0]-geometry[j,0])+dy[j]*(CP[i,1]-geometry[j,1]))/l[j] #define xi term
-        eta = (-dy[j]*(CP[i,0]-geometry[j,0])+dx[j]*(CP[i,1]-geometry[j,1]))/l[j] #define eta term
-        
-        phi = np.arctan2(eta*l[j],eta**2+xi**2-xi*l[j]) #define phi term
-        psi = 0.5*np.log((xi**2+eta**2)/((xi-l[j])**2+eta**2)) #define psi term
+if data['view']['CL_hat_distributions']: #plot CL hat distributions if desired by user
+    plt.figure('CL_hat_distributions')
 
-        P1 = np.array([[dx[j],-dy[j]],[dy[j],dx[j]]]) #calculate first part of P array
-        P2 = np.array([[(l[j]-xi)*phi+eta*psi,xi*phi-eta*psi],
-            [eta*phi-(l[j]-xi)*psi-l[j],-eta*phi-xi*psi+l[j]]]) #calculate second part of P array
-        P = (1/(2*np.pi*l[j]**2))*np.matmul(P1,P2) #create P array by matrix multiplying parts
+if data['view']['CL_tilde_distributions']: #plot CL tilde distributions if desired by user
+    plt.figure('CL_tilde_distributions')
 
-        A[i,j] += dx[i]*P[1,0]/l[i]-dy[i]*P[0,0]/l[i] #calculat current cell of A array
-        A[i,j+1] += dx[i]*P[1,1]/l[i]-dy[i]*P[0,1]/l[i] #start calculating next cell of A array
-
-A[n-1,0] = 1.0 #define edge of A array based on kutta condition
-A[n-1,n-1] = 1.0 #define corner of A array based on kutta condition
-
-#use B array to solve for gamma for all angles of attack
-B = np.zeros((n,n_alpha)) #initialize B column(s)
-gamma = np.zeros((n,n_alpha))  #initialize gamma column(s)
-for i in range(n_alpha):
-    B[:-1,i] = v_inf*((geometry[1:,1]-geometry[:-1,1])*np.cos(alpha[i])-
-        (geometry[1:,0]-geometry[:-1,0])*np.sin(alpha[i]))/[l[:]] #create B array for current angles of attack
-    gamma[:,i] = np.linalg.solve(A,B[:,i]) #solve for gamma asoociated with current angle of attack
-
-#find coeffecient of lift
-C_L = np.zeros(n_alpha) #initialize lift coefficient array
-for i in range(n_alpha):
-    C_L[i] = np.sum(l[:]*(gamma[:-1,i]+gamma[1:,i])/v_inf) #find value of current iteration of lift coefficient array
-
-#find leading edge moment coefficient array
-C_mLE = np.zeros(n_alpha) #initialize leading edge moment coefficient array
-for i in range(n_alpha):
-    C_mLE[i] = -1/3*np.sum(l[:]/v_inf*((2*geometry[:-1,0]*gamma[:-1,i]+geometry[1:,0]*gamma[:-1,i]+geometry[:-1,0]*gamma[1:,i]+2*geometry[1:,0]*gamma[1:,i])*np.cos(alpha[i])
-    +(2*geometry[:-1,1]*gamma[:-1,i]+geometry[1:,1]*gamma[:-1,i]+geometry[:-1,1]*gamma[1:,i]+2*geometry[1:,1]*gamma[1:,i])*np.sin(alpha[i]))) #find value of current iteration of lift coefficient array
-
-# Find quarter chord moment coefficient array
-C_mQC = np.zeros(n_alpha)  #initialize quarter chord moment coefficient array
-QC_x = geometry[:,0]-0.25 #Compute the moment arm relative to the quarter chord
-for i in range(n_alpha):
-    C_mQC[i] = -1/3 * np.sum(
-        l[:]/v_inf*(
-            (2*QC_x[:-1]*gamma[:-1, i]+QC_x[:-1]*gamma[1:,i]+ 
-            QC_x[1:]*gamma[:-1,i]+2*QC_x[1:]*gamma[1:,i])*np.cos(alpha[i])+
-            (2*geometry[:-1,1]*gamma[:-1,i]+geometry[1:,1]*gamma[:-1,i]+ 
-            geometry[:-1,1]*gamma[1:,i]+2*geometry[1:,1]*gamma[1:,i])*np.sin(alpha[i])
-        )
-    )  # find value of current iteration of quarter chord moment coefficient array
-
-#print out calculated coefficients for verification
-print(f"a: {alpha*180/np.pi}\n")
-print(f"C_L: {C_L}\n")
-print(f"C_mLE: {C_mLE}\n")
-print(f"C_mQC: {C_mQC}\n")
-
-#Plot airfoil
-plt.figure(1)
-plt.plot(geometry[:,0],geometry[:,1])
-plt.axis("equal")
-plt.title('Airfoil Geometry')
-
-#Plot coefficient of lift
-plt.figure(2)
-plt.plot(alpha*180/np.pi,C_L)
-plt.title('Lift Coefficient ($C_{L}$)')
-plt.xlabel("Angle ($^{o}$)");plt.ylabel("$C_{L}$",rotation='horizontal')
-
-#Plot leading edge moment coefficient
-plt.figure(3)
-plt.plot(alpha*180/np.pi,C_mLE)
-plt.title('Coefficient of Leading Edge Moment ($C_{mLE}$)')
-plt.xlabel("Angle ($^{o}$)");plt.ylabel("$C_{mLE}$",rotation='horizontal')
-
-#Plot quarter-chord moment coefficient
-plt.figure(4)
-plt.plot(alpha*180/np.pi,C_mQC)
-plt.title('Coefficient of Quarter-Chord Moment ($C_{mc/4}$)')
-plt.xlabel("Angle ($^{o}$)");plt.ylabel("$C_{mc/4}$",rotation='horizontal')
 plt.show()
-'''
